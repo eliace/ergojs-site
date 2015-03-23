aeec5d793ed96fd2053afacead47fed50803f51f
unknown <YuKodanev@SYKAV-WS-LT-021.domain.local>
2015-03-23 18:42:17 +0300

    ~

--- samples/widgets/menu/nav-menu.js
+++ samples/widgets/menu/nav-menu.js
@@ -265,3 +265,56 @@ var w = $.ergo({
 */
 
 w.render('#sample');
+
+
+
+
+
+
+var data = [{
+	title: 'Главная',
+	icon: 'fa-dashboard',
+	name: 'dashboard'
+}, {
+	title: 'Товары',
+	icon: 'fa-dashboard',
+	name: 'dashboard'
+}, {
+	title: 'Организации',
+	icon: 'fa-dashboard',
+	name: 'dashboard'
+}, {
+	title: 'Личный кабинет',
+	icon: 'fa-dashboard',
+	name: 'dashboard'
+}];
+
+
+
+var w = $.ergo({
+	etype: 'navigation',
+	style: {'margin-top': 32},
+	$content: {
+		items: [{
+			etype: 'list',
+			cls: 'nav-menu',
+			mixins: ['selectable'],
+			data: data,
+			defaultItem: {
+				$content: {
+					etype: 'link',
+					$content: {
+						etype: '&text',
+						format: '#{title}'
+					}
+				},
+				actions: {
+					'jquery:click': 'select'
+				}				
+			}
+		}]
+	}
+});
+
+
+w.render('#sample');
