11f3be9c31151168bf43f13c7cb9bdf4c70798e9
eliace <eliace@mail.ru>
2015-09-15 22:31:21 +0300

    ~

--- js/examples-context.js
+++ js/examples-context.js
@@ -76,13 +76,13 @@ $context.section_begin = function(block) {
 					this.events.rise('toggleExpand');
 				}
 			},
-			$jsfiddle: {
-				etype: 'icon',
-				as: 'contextual fa-jsfiddle pull-right action',
-				onClick: function() {
-					this.events.rise('jsFiddle');
-				}
-			}
+			// $jsfiddle: {
+			// 	etype: 'icon',
+			// 	as: 'contextual fa-jsfiddle pull-right action',
+			// 	onClick: function() {
+			// 		this.events.rise('jsFiddle');
+			// 	}
+			// }
 		},
 		$description: {
 			cls: 'description',
@@ -154,84 +154,6 @@ $context.section_begin = function(block) {
 		},
 
 
-		onJsFiddle: function(e) {
-
-			var sample = $context.data('sample');
-
-			var path = (block) ? sample.name+'/'+block+'.js' : sample.name+'.js';
-
-			$.ajax({
-				url:'samples/'+path,
-				dataType:"text",
-				async: false,
-				success:function(data){
-
-					console.log(data);
-
-					var js = data;// Ergo.escapeHtml(data).replace(/\t/g, '  ');
-
-					var form = $.ergo({
-						etype: 'html:form',
-						action: 'http://jsfiddle.net/api/post/jquery/2.1.3/',
-						defaultItem: {
-							etype: 'html:input',
-							hidden: true
-						},
-//						id: 'jsfiddle-form',
-						items: [{
-							name: 'title',
-							value: sample.name
-						}, {
-							name: 'wrap',
-							value: 'd'
-						}, {
-							name: 'html',
-							etype: 'html:textarea',
-							value: '<script src="http://ergojs.com/lib/ergojs-core.js" type="text/javascript"></script>\n<script src="http://ergojs.com/lib/ergojs-widgets-all.js" type="text/javascript"></script>\n<script>Ergo.widgets.Icon.prototype.defaults.cls = "icon fa fa-fw";</script>\n<div id="sample" class="padding"></div>'
-						}, {
-							name: 'js',
-							etype: 'html:textarea',
-							value: js
-						}, {
-							name: 'resources',
-							value: 'http://ergojs.com/js/examples-context.js,http://ergojs.com/js/examples-data.js,http://ergojs.com/lib/ergojs.css,https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'
-						}]
-					});
-
-					form.render('body');
-
-					form.el.attr('target', '_blank');
-					//
-					// window.open('http://jsfiddle.net', 'TheWindow');
-
-					form.el.submit();
-
-//					form._destroy();
-
-/*
-					$.ajax({
-						url: 'http://jsfiddle.net/api/post/jquery/2.1.3/',
-						type: 'post',
-						data: {
-							wrap: 'd',
-							title: sample.name,
-							dtd: 'html 4',
-							resources: 'http://ergojs.com/lib/ergojs-core.js',
-							js: js,
-							panel_css: 0,
-							panel_html:	0,
-							panel_js: 0
-						},
-						success: function(data) {
-							console.log(data)
-						}
-					});
-*/
-				}
-			});
-
-//			e.stop();
-		}
 
 
 	});
@@ -257,8 +179,84 @@ $context.section_end = function(block) {
 		$tabs: {
 			cls: 'code-tabs',
 			layout: 'hbox',
-			items: [{text: 'JS', state: 'selected'}, 'CSS']
+			defaultItem: {
+				include: 'icon:before',
+				icon: 'fa-jsfiddle',
+				as: 'action',
+				onClick: 'action:jsFiddle'
+			},
+			items: ['jsFiddle']//{text: 'JS', state: 'selected'}, 'CSS']
+		},
+
+
+		onJsFiddle: function(e) {
+
+			var sample = $context.data('sample');
+
+			var path = (block) ? sample.name+'/'+block+'.js' : sample.name+'.js';
+
+			$.ajax({
+				url:'samples/'+path,
+				dataType:"text",
+				async: false,
+				success:function(data){
+
+					var js = data;// Ergo.escapeHtml(data).replace(/\t/g, '  ');
+
+					var form = $.ergo({
+						etype: 'html:form',
+						action: 'http://jsfiddle.net/api/post/jquery/2.1.3/',
+						defaultItem: {
+							etype: 'html:input',
+							hidden: true
+						},
+//						id: 'jsfiddle-form',
+						items: [{
+							name: 'title',
+							value: sample.name
+						}, {
+							name: 'wrap',
+							value: 'd'
+						}, {
+							name: 'html',
+							etype: 'html:textarea',
+							value: '<script src="http://ergojs.com/lib/ergojs-core.js" type="text/javascript"></script>\n'
+								+'<script src="http://ergojs.com/lib/ergojs-widgets-all.js" type="text/javascript"></script>\n'
+								+'<script src="http://ergojs.com/js/examples-context.js" type="text/javascript"></script>\n'
+								+'<script src="http://ergojs.com/js/examples-data.js" type="text/javascript"></script>\n'
+								+'<link href="http://fonts.googleapis.com/css?family=Roboto:400,500,600,700&subset=latin,cyrillic" rel="stylesheet" type="text/css">\n'
+								+'<link href="http://ergojs.com/css/layout.css" rel="stylesheet" type="text/css">\n'
+								+'<link href="http://ergojs.com/css/style.css" rel="stylesheet" type="text/css">\n'
+								+'<script>Ergo.widgets.Icon.prototype.defaults.cls = "icon fa fa-fw";</script>\n\n'
+								+'<div id="sample" class="padding"></div>'
+						}, {
+							name: 'js',
+							etype: 'html:textarea',
+							value: js
+						}, {
+							name: 'resources',
+							value: 'http://ergojs.com/lib/ergojs.css,https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'
+						}]
+					});
+
+					form.render('body');
+
+					form.el.attr('target', '_blank');
+					//
+					// window.open('http://jsfiddle.net', 'TheWindow');
+
+					form.el.submit();
+
+					form._destroy();
+
+				}
+			});
+
+//			e.stop();
 		}
+
+
+
 	});
 
 };
